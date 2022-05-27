43e227bfe1912cc7227117e1c7c381a9cce0658a
Mathyu Cardozo <68485205+xMathyu@users.noreply.github.com>
2022-05-27 00:17:08 -0500

    Full language button added

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var check=document.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id=check.checked;
+    if(id==true){
+        location.href="../../indexES.html";
+        document.getElementsByClassName('es').style = "color: #00a1ff";
+        document.getElementsByClassName('en').style= "background-color: #333";
+    }
+    else{
+        location.href="../../index.html"
+        document.getElementsByClassName('en').style = "color: #00a1ff";
+        document.getElementsByClassName('es').style= "color: #333";
+    }
+}
